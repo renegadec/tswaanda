3ef20cb090be63bcf6052775febd8a020cdad1aa
africoiner <confidencenyirenda7@gmail.com>
2022-10-07 15:58:46 +0200

    small changes

--- tswaanda/src/App.jsx
+++ tswaanda/src/App.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { FiSettings } from 'react-icons/fi';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import styles from "./style";
+import DashboardApp from './components/dashboard/DashboardApp';
 
 import {
   Navbar,
@@ -18,41 +17,40 @@ import {
 
 const App = () => (
   <div>
-    <BrowserRouter>
-      <div className='flex relative dark:bg-main-dark-bg'>
-        <div className="fixed right-4 bottom-4" style={{zIndex: '1000'}}>
-          <TooltipComponent content="Settings" position='Top'>
-
-          </TooltipComponent>
+      <div className="bg-primary w-full overflow-hidden">
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Navbar />
+          </div>
         </div>
-      </div>
+
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Hero />
+          </div>
+        </div>
+
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Stats />
+            <About />
+            <Services />
+            <AppFeatures />
+            <Testimonials />
+            <CTA />
+            <Footer />
+          </div>
+        </div>
+    </div>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path='/dashboard' element={<DashboardApp />} />
+      </Routes>
     </BrowserRouter>
-    <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Navbar />
-      </div>
-    </div>
-
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
-      </div>
-    </div>
-
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Stats />
-        <About />
-        <Services />
-        <AppFeatures />
-        <Testimonials />
-        <CTA />
-        <Footer />
-      </div>
-    </div>
-  </div>
   </div>
 );
 
 export default App;
+
+
