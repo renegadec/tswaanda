f71ad592b93460f44a2d5746733f62b84491d198
africoiner <confidencenyirenda7@gmail.com>
2022-10-16 10:34:15 +0200

    added functionality to contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import emailjs from '@emailjs/browser';
 import styles from "../style";
 
 const Contact = () => {
+
+    const [userName, setUserName] = useState('');
+    const [userEmail, setUserEmail] = useState('');
+    const [userMessage, setUserMessage] = useState('');
+
+
+    const form = useRef();
+
+    const sendMail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('', '', form.current, '')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+
+        setUserName('');
+        setUserEmail('');
+        setUserMessage('');
+    };
+
     return (
         <div>
             <section className="text-gray-700 body-font relative">
@@ -13,57 +37,60 @@ const Contact = () => {
                     </p>
                 </div>
                 <div className="lg:w-1/2 md:w-2/3 mx-auto">
-                    <div className="flex flex-wrap -m-2">
-                    <div className="p-2 w-1/2">
-                        <div className="relative">
-                        <label for="name" className="leading-7 text-sm text-gray-600">
-                            Name
-                        </label>
-                        <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            className="w-full bg-white input input-bordered input-success text-gray-700 py-1 px-3 duration-200 ease-in-out"
-                        />
+                    <form ref={form} onSubmit={sendMail} className="flex flex-wrap -m-2">
+                        <div className="p-2 w-1/2">
+                            <div className="relative">
+                            <label htmlFor="name" className="leading-7 text-sm text-gray-600">
+                                Name
+                            </label>
+                            <input
+                                type="text"
+                                name="from_name"
+                                onChange = {e => setUserName(e.target.value)}
+                                value = {userName}
+                                className="w-full bg-white input input-bordered input-success text-gray-700 py-1 px-3 duration-200 ease-in-out"
+                            />
+                            </div>
                         </div>
-                    </div>
-                    <div className="p-2 w-1/2">
-                        <div className="relative">
-                        <label
-                            for="email"
-                            className="leading-7 text-sm text-gray-600"
-                        >
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            className="w-full bg-white  input input-bordered input-success text-gray-700 py-1 px-3 duration-200 ease-in-out"
-                        />
+                        <div className="p-2 w-1/2">
+                            <div className="relative">
+                            <label
+                                htmlFor="email"
+                                className="leading-7 text-sm text-gray-600"
+                            >
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                name="from_email"
+                                onChange = {e => setUserEmail(e.target.value)}
+                                value = {userEmail}
+                                className="w-full bg-white  input input-bordered input-success text-gray-700 py-1 px-3 duration-200 ease-in-out"
+                            />
+                            </div>
                         </div>
-                    </div>
-                    <div className="p-2 w-full">
-                        <div className="relative">
-                        <label
-                            for="message"
-                            className="leading-7 text-sm text-gray-600"
-                        >
-                            Message
-                        </label>
-                        <textarea
-                            id="message"
-                            name="message"
-                            className="w-full textarea textarea-success bg-white"
-                        ></textarea>
+                        <div className="p-2 w-full">
+                            <div className="relative">
+                            <label
+                                htmlFor="message"
+                                className="leading-7 text-sm text-gray-600"
+                            >
+                                Message
+                            </label>
+                            <textarea
+                                name="message"
+                                onChange = {e => setUserMessage(e.target.value)}
+                                value = {userMessage}
+                                className="w-full textarea textarea-success bg-white"
+                            ></textarea>
+                            </div>
                         </div>
-                    </div>
-                    <div className="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
-                        <button className={`flex font-poppins font-normal cursor-pointer text-[16px] text-white btn btn-outline btn-primary`}>
-                        Send Message
-                        </button>
-                    </div>
-                    </div>
+                        <div className="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
+                            <button className={`flex font-poppins font-normal cursor-pointer text-[16px] text-white btn btn-outline btn-primary`}>
+                            Send Message
+                            </button>
+                        </div>
+                    </form>
                 </div>
                 </div>
             </section>
